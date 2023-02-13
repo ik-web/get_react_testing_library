f0df61e10c30b836c129f79fd7003d868ec81383
Ihor Kuchin <vedunigor@gmail.com>
2023-02-13 16:07:33 +0200

    Added word-wrap for todo item

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,10 +1,13 @@
+import './Todo.css';
+
 export const Todo = ({ todo, todoIndex }) => {
+  const todoText = `${todoIndex}. ${todo.task}`;
   return (
     <div
       key={todo.id}
       className={`todoItem ${todo.completed && 'todoItem_active'}`}
     >
-      {todoIndex}. {todo.task}
+      {todoText}
     </div>
   );
 };
